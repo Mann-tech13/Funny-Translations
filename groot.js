24f435c9557ec4e6689399f7b6e91102c1101748
Mann-tech13 <mannjshah01@gmail.com>
2020-12-09 17:50:27 +0530

    update upto 09.12.2020

---
+++ groot.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn");
+var txtInput = document.querySelector("#txtInput")
+var output = document.querySelector("#output");
+var url = "https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(text) 
+{
+  return url + "?" + "text=" + text
+}
+
+function errorHandler(error)
+{
+  console.log("Error occured", error);
+  alert("Something wrong with server! \nTry again later!!")
+}
+
+function clickHandler()
+{
+  fetch(getTranslationURL(txtInput.value))
+    .then(response => response.json())
+    .then(json => {output.innerText = json.contents.translated})
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click",clickHandler);
